1313b696961035778bcbfb6608d84264cfd73c2b
leeleegabriel <lgv0001@auburn.edu>
2019-10-04 22:50:14 -0500

    completed reading/writing

--- parrot.py
+++ parrot.py
@@ -10,7 +10,7 @@ import random
 import sys
 import string
 
-GITHUB_API="https://api.github.com/gists"
+GITHUB_API="https://api.github.com"
 
 
 def main(site, username, read):
@@ -22,23 +22,40 @@ def main(site, username, read):
 
 def readPass(site):
 	try:
-		pass
-		# [print('{ln}') for ln in gist.search(git_username).content(name='PARROT_PASSWORD_{site}').split(':')[:-1]]
+		headers = {'Authorization':f'token {git_token}'}
+		params = {'username': git_username, 'filename': f'PARROT_PASSWORD_{site}'}
+		#res = requests.get(GITHUB_API+'/search/gists', headers=headers, params=params)
+		res = requests.get(f'{GITHUB_API}/gists', headers=headers, params=params)
+		j = json.loads(res.text)
+		for gist in j:
+			if f'PARROT_PASSWORD_{site}' in gist['files'].keys():
+				cred_dict = gist['files'][f'PARROT_PASSWORD_{site}']
+				res = requests.get(cred_dict['raw_url'], headers=headers)
+				creds = res.text.split(':')
+				print(f'Retrieved credentials for {site} from gist.github.com')
+				print(f'username: {creds[0]}')
+				print(f'password: {creds[1]}')
+				print(f"gist ID: {gist['id']}")		
+	#	gist_id = res.body.items[0].id
+	# 	res = requests.get(f'{GIT_URL}/gists/{gist_id}', headers=header)
+	# 	content = res.files[0].content
+	# 	creds = content.split(':')
+	# 	print(f'username: {creds[0]}')
+	# 	print(f'password: {creds[1]}')
 	except Exception as e:
 		print(f'Error Reading: {e}')
 
 
 def writePass(username, site):
 	try:
-		password=''.join(random.choice(string.ascii_lowercase) for i in range(16))
-		headers={'Authorization':f'token {git_token}'}
-		params={'scope':'gist'}
-		payload={"description":"PARROT PASSWORD","public":True,"files":{f"{site}":{"content":f"{username}:{password}"}}}
-		res=requests.post(GITHUB_API,headers=headers,params=params,data=json.dumps(payload))
-		j=json.loads(res.text)
+		password = ''.join(random.choice(string.ascii_lowercase) for i in range(16))
+		headers = {'Authorization':f'token {git_token}'}
+		params = {'scope':'gist'}
+		payload = {'description':'PARROT PASSWORD','public':True,'files':{f'PARROT_PASSWORD_{site}':{'content':f'{username}:{password}'}}}
+		res = requests.post(f'{GITHUB_API}/gists', headers=headers, params=params,data=json.dumps(payload))
+		j = json.loads(res.text)
 		print(f"Gist saved with ID {j['id']} at {j['html_url']}")
 		print(f"Password is {password}")
-
 	except Exception as e:
 		print(f'Error Writing: {e}')
 
@@ -56,7 +73,7 @@ if __name__ == '__main__':
 	username = args.username
 	read = args.read
 
-	if read is not None and site is not None and ((read and username is not None) or not read):
+	if read is not None and site is not None and ((not read and username is not None) or read):
 		main(site, username, read)
 	else:
 		print('learn to read asshole.')
